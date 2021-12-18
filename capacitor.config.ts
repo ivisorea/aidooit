b6535694b564b6655c1727c5264d08355d4df057
Ivis Orea <ivisorea@Iviss-MBP.fritz.box>
2021-12-18 19:12:00 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'aidooit_ionic_react_app',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
